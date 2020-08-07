e0a4ed1f8c4c71cda1948400a0c7f96d47626de6
Alex Tamoykin <alex@rightbalance.io>
2020-08-06 18:49:55 -0700

    Initial commit

---
+++ tdd/tdd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tdd
+//
+//  Created by Alex Tamoykin on 8/6/20.
+//  Copyright © 2020 Right Balance. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

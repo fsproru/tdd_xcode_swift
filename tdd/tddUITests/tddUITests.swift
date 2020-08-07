41c3ee2af224caeb3aaa85e742078f4337c6f330
Alex Tamoykin <alex@rightbalance.io>
2020-08-07 02:21:48 -0700

    Remove the tokens

--- tdd/tddUITests/tddUITests.swift
+++ tdd/tddUITests/tddUITests.swift
@@ -16,11 +16,11 @@ class tddUITests: XCTestCase {
 
         let tablesQuery = XCUIApplication().tables
         let refactorElementsQuery = tablesQuery.cells.otherElements.containing(.staticText, identifier:"Refactor")
-        XCTAssertTrue(refactorElementsQuery/*@START_MENU_TOKEN@*/.staticTexts["❌"]/*[[".cells.staticTexts[\"❌\"]",".staticTexts[\"❌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        XCTAssertTrue(refactorElementsQuery.staticTexts["❌"].exists)
 
         refactorElementsQuery.buttons["Complete"].tap()
 
-        XCTAssertFalse(refactorElementsQuery/*@START_MENU_TOKEN@*/.staticTexts["❌"]/*[[".cells.staticTexts[\"❌\"]",".staticTexts[\"❌\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.exists)
+        XCTAssertFalse(refactorElementsQuery.staticTexts["❌"].exists)
         XCTAssertTrue(refactorElementsQuery.staticTexts["✅"].exists)
     }
 }
